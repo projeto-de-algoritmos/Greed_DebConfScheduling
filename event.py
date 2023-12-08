a0776a49f57df04436b08fbbbe08a4f6ead5009e
Thais <rotinasoftware@gmail.com>
2023-12-08 17:20:54 -0300

    Função que executa o interval scheduling

--- event.py
+++ event.py
@@ -20,3 +20,16 @@ def sort_by_finish_time(agenda):
     sorted_events_as_dicts = [event.as_dict() for event in sorted_events]
     
     return sorted_events_as_dicts
+
+def find_best_schedule(agenda):
+    # Encontra os melhores horário levendo em consideração o menor tempo de ócio
+    sorted_events = sort_by_finish_time(agenda)
+    best_programming = []
+
+    for event in sorted_events:
+        if best_programming:
+            if best_programming[-1]['end_time'] <= event['start_time']:
+                best_programming.append(event)
+        else:
+            best_programming.append(event)
+    return best_programming

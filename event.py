3a9f154ddbc3d2f7e6c3c5db1e9d6e3ee6a44693
Thais <rotinasoftware@gmail.com>
2023-12-09 00:54:48 -0300

    Pega dados do json para encontrar a melhor programação

--- event.py
+++ event.py
@@ -1,22 +1,22 @@
-class Event:
-    def __init__(self, name, start, end, date):
+import json
 
-        self.name = name
-        self.start = start
-        self.end = end
-        self.date = date
+class Event:
+    def __init__(self, titulo, horario_inicio, horario_fim):
+
+        self.titulo = titulo
+        self.horario_inicio = horario_inicio
+        self.horario_fim = horario_fim
 
     def as_dict(self):
-        return {'name': self.name, 'start_time': self.start, 'end_time': self.end, 'date': self.date}
-
+        return {'titulo': self.titulo, 'horario_inicio': self.horario_inicio, 'horario_fim': self.horario_fim}
 
 def sort_by_finish_time(agenda):
     # Ordena eventos por ordem de término e transforma em dicionário
     event_objects = []
     for i in agenda:
-        event_objects.append(Event(i['titulo'], i['horario_inicio'], i['horario_fim'], i['data']))
+        event_objects.append(Event(i['titulo'], i['horario_inicio'], i['horario_fim']))
     
-    sorted_events = sorted(event_objects, key=lambda event: event.end)
+    sorted_events = sorted(event_objects, key=lambda event: event.horario_fim)
     sorted_events_as_dicts = [event.as_dict() for event in sorted_events]
     
     return sorted_events_as_dicts
@@ -28,8 +28,14 @@ def find_best_schedule(agenda):
 
     for event in sorted_events:
         if best_programming:
-            if best_programming[-1]['end_time'] <= event['start_time']:
+            if best_programming[-1]['horario_fim'] <= event['horario_inicio']:
                 best_programming.append(event)
         else:
             best_programming.append(event)
     return best_programming
+
+def get_data(day):
+    with open('./schedule.json', 'r') as fp:
+        data_json = json.load(fp)
+    
+    return find_best_schedule(data_json[day])
